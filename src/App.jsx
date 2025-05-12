6e1e43cfbda9f0281a92f3c291b4635f34ab274e
Nicolás Santísima Trinidad Santín <sannicolas2121@outlook.com>
2025-05-12 21:04:33 +0200

    Improving styling 🖌️

--- src/App.jsx
+++ src/App.jsx
@@ -24,7 +24,7 @@ function App() {
       'display': 'none'
     },
     '.cm-content': {
-      'background-color': '#313131',
+      'background-color': '#09090b',
       'font-family': 'Fira Code',
       'padding': '9px'
     },
@@ -38,7 +38,7 @@ function App() {
       'height': 'fit-content',
       'padding': '5px',
       'border-radius': '4px',
-      'background-color': '#313131',
+      'background-color': '#09090b',
       'background-image': 'none',
       'font-family': 'Fira Code'
     },
@@ -239,23 +239,23 @@ function App() {
 
   return (
     <>
-      <header style={theme === 'dark' ? { backgroundColor: '#313131', borderBottom: '1.5px solid #8e8e8e' } : { backgroundColor: 'white', borderBottom: '1.5px solid black', color: 'black' }}>
+      <header style={theme === 'dark' ? { backgroundColor: '#09090b', borderBottom: '1.5px solid #8e8e8e' } : { backgroundColor: 'white', borderBottom: '1.5px solid black', color: 'black' }}>
         <h1 style={{ color: '#38b6ff' }}>Sam<span style={{ color: 'yellow' }}>JS</span></h1>
         <div className="buttons">
           <Save theme={theme} code={code} editor={editor} />
-          <button id='packageSearch' className='hint--bottom-left hint--bounce hint--rounded tool-button button' aria-label='Search packages' onClick={() => dialogOpen ? setDialogOpen(false) : setDialogOpen(true)}><img src='/package.svg' alt='Copy shareable link' /></button>
-          <button id='share' className='hint--bottom-left hint--bounce hint--rounded tool-button button' aria-label={copyToolTip} disabled={code === ''} onClick={copyShareableLink}><img src='/share.svg' alt='Copy shareable link' /></button>
+          <button id='packageSearch' className='hint--bottom-left hint--bounce hint--rounded tool-button button' aria-label='Search packages' onClick={() => dialogOpen ? setDialogOpen(false) : setDialogOpen(true)} style={theme === 'dark' ? { color: 'white' } : { color: 'black' }}><svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="CURRENTCOLOR"><path d="M440-183v-274L200-596v274l240 139Zm80 0 240-139v-274L520-457v274Zm-80 92L160-252q-19-11-29.5-29T120-321v-318q0-22 10.5-40t29.5-29l280-161q19-11 40-11t40 11l280 161q19 11 29.5 29t10.5 40v318q0 22-10.5 40T800-252L520-91q-19 11-40 11t-40-11Zm200-528 77-44-237-137-78 45 238 136Zm-160 93 78-45-237-137-78 45 237 137Z" /></svg></button>
+          <button id='share' className='hint--bottom-left hint--bounce hint--rounded tool-button button' aria-label={copyToolTip} disabled={code === ''} onClick={copyShareableLink} style={theme === 'dark' ? { color: 'white' } : { color: 'black' }}><svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="CURRENTCOLOR"><path d="M680-80q-50 0-85-35t-35-85q0-6 3-28L282-392q-16 15-37 23.5t-45 8.5q-50 0-85-35t-35-85q0-50 35-85t85-35q24 0 45 8.5t37 23.5l281-164q-2-7-2.5-13.5T560-760q0-50 35-85t85-35q50 0 85 35t35 85q0 50-35 85t-85 35q-24 0-45-8.5T598-672L317-508q2 7 2.5 13.5t.5 14.5q0 8-.5 14.5T317-452l281 164q16-15 37-23.5t45-8.5q50 0 85 35t35 85q0 50-35 85t-85 35Zm0-80q17 0 28.5-11.5T720-200q0-17-11.5-28.5T680-240q-17 0-28.5 11.5T640-200q0 17 11.5 28.5T680-160ZM200-440q17 0 28.5-11.5T240-480q0-17-11.5-28.5T200-520q-17 0-28.5 11.5T160-480q0 17 11.5 28.5T200-440Zm480-280q17 0 28.5-11.5T720-760q0-17-11.5-28.5T680-800q-17 0-28.5 11.5T640-760q0 17 11.5 28.5T680-720Zm0 520ZM200-480Zm480-280Z" /></svg></button>
           <button id='theme' className='hint--bottom-left hint--bounce hint--rounded button' aria-label='Switch theme' onClick={changeTheme}>{theme === 'dark' ? '🌙' : '☀️'}</button>
           <Auth />
         </div>
       </header>
       <main >
         <div id='editor' ref={editorDiv} style={theme === 'dark' ? { '--border': '#8e8e8e' } : { '--border': 'black' }} />
-        <div id='log' style={theme === 'dark' ? { backgroundColor: '#313131' } : { backgroundColor: 'white' }}>{log}</div>
+        <div id='log' style={theme === 'dark' ? { backgroundColor: '#09090b' } : { backgroundColor: 'white' }}>{log}</div>
       </main>
-      <dialog open={dialogOpen} style={theme === 'dark' ? { '--background': '#333333', '--color': 'white', '--border': 'white' } : { '--background': 'white', '--color': 'black', '--border': 'black' }} className='ui-dialog'>
+      <dialog open={dialogOpen} style={theme === 'dark' ? { '--background': '#09090b', '--color': 'white', '--border': '#151517' } : { '--background': 'white', '--color': 'black', '--border': 'black' }} className='ui-dialog'>
         <h1>Search a package 📦</h1>
-        <input type="text" onKeyDown={searchPackage} placeholder='Package...' />
+        <input type="text" onKeyDown={searchPackage} placeholder='Package...' style={theme === 'dark' ? { '--background': '#09090b', '--border': '#a1a1a5' } : { '--background': 'white', '--border': '#b8b8bf' }} />
         <div className="packages">
           {packages}
         </div>
--- src/components/Save.jsx
+++ src/components/Save.jsx
@@ -42,7 +42,7 @@ function Save(props) {
                     }
                 })
                 setDialogOpen(false)
-            }}>{item.code.split('\n')[0]}</span><div className='buttons snippet-buttons'><button className='tool-button button' aria-label='Remove snippet' onClick={
+            }}>{item.code.split('\n')[0]}</span><div className='buttons snippet-buttons'><button className='tool-button button' aria-label='Remove snippet' style={theme === 'dark' ? { color: 'white' } : { color: 'black' }} onClick={
                 async () => {
                     editor.current.dispatch({
                         changes: {
@@ -51,7 +51,7 @@ function Save(props) {
                         }
                     })
                 }
-            }><img src="/add.svg" alt="Add the content of the snippet to the editor" /></button><button className='tool-button button' aria-label='Remove snippet' onClick={
+            }><svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="CURRENTCOLOR"><path d="M440-440H200v-80h240v-240h80v240h240v80H520v240h-80v-240Z" /></svg></button><button className='tool-button button' aria-label='Remove snippet' onClick={
                 async () => {
                     const { error } = await supabase.from('code').delete().eq('id', item.id)
                     if (error) {
@@ -85,10 +85,10 @@ function Save(props) {
 
     return (
         <>
-            <button id='packageSearch' className='hint--bottom-left hint--bounce hint--rounded tool-button button' aria-label='Code snippets' disabled={!userId} onClick={() => setDialogOpen(dialogOpen ? false : true)}>
-                <img src='/code.svg' alt='Copy shareable link' />
+            <button id='packageSearch' className='hint--bottom-left hint--bounce hint--rounded tool-button button' aria-label='Code snippets' disabled={!userId} onClick={() => setDialogOpen(dialogOpen ? false : true)} style={theme === 'dark' ? { 'color': 'white' } : { 'color': 'black' }}>
+                <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="12px" fill="CURRENTCOLOR"><path d="M320-240 80-480l240-240 57 57-184 184 183 183-56 56Zm320 0-57-57 184-184-183-183 56-56 240 240-240 240Z" /></svg>
             </button>
-            <dialog open={dialogOpen} style={theme === 'dark' ? { '--background': '#333333', '--color': 'white', '--border': 'white' } : { '--background': 'white', '--color': 'black', '--border': 'black' }} className='ui-dialog'>
+            <dialog open={dialogOpen} style={theme === 'dark' ? { '--background': '#09090b', '--color': 'white', '--border': '#151517' } : { '--background': 'white', '--color': 'black', '--border': 'black' }} className='ui-dialog'>
                 <h1>Code snippets</h1>
                 <button className="dialog-button hint--bottom-left hint--bounce hint--rounded" onClick={saveSnippet} disabled={snippets.length > 4 || code.trim() < 1} aria-label={snippets.length > 4 ? 'Max. 4 snippets, delete one' : ''}>Save current code</button>
                 <div className="snippets">
