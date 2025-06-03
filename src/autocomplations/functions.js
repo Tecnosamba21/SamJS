4ac4eeb538b93e85f4391128edec12ad72917ade
Nicolás Santísima Trinidad Santín <sannicolas2121@outlook.com>
2025-06-03 17:18:03 +0200

    Refactorizing some code ⭐

---
+++ src/autocomplations/functions.js
@@ -0,0 +1,16 @@
+export const functions = [
+    { "label": "forEach", "type": "function", "info": "Iterate the elements of an array" },
+
+    { "label": "isNaN", "type": "function", "info": "Check if a value is NaN" },
+    { "label": "isFinite", "type": "function", "info": "Check if a value is a finite number" },
+    { "label": "parseFloat", "type": "function", "info": "Parse a string to a floating-point number" },
+    { "label": "parseInt", "type": "function", "info": "Parse a string to an integer" },
+    { "label": "encodeURI", "type": "function", "info": "Encode a URI" },
+    { "label": "encodeURIComponent", "type": "function", "info": "Encode a URI component" },
+    { "label": "decodeURI", "type": "function", "info": "Decode a URI" },
+    { "label": "decodeURIComponent", "type": "function", "info": "Decode a URI component" },
+    { "label": "setTimeout", "type": "function", "info": "Call a function after a delay" },
+    { "label": "clearTimeout", "type": "function", "info": "Clear a timeout" },
+    { "label": "setInterval", "type": "function", "info": "Call a function repeatedly after a delay" },
+    { "label": "clearInterval", "type": "function", "info": "Clear an interval" },
+]
---
+++ src/autocomplations/variables.js
@@ -0,0 +1,5 @@
+export const variables = [
+    { "label": "undefined", "type": "variable", "info": "Primitive value" },
+    { "label": "Infinity", "type": "variable", "info": "Special numeric value" },
+    { "label": "NaN", "type": "variable", "info": "Not-a-Number value" },
+]
